911feef3c1f5bdef7663efbebe9eb9f28f7fd4b7
Manikant Singh <manikant2869@gmail.com>
2023-01-22 05:44:02 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { useState } from 'react';
+import './App.css';
+const button = [9,8,7,6,5,4,3,2,1,0,'.']
+const functionButton = ['*','/','+','-']
 function App() {
+  const [valueToCalculate,setValueToCalculate] = useState(""); 
+  function handleClick(e){
+    const name = e.target.name ; 
+    if(name === 'equal'){
+      const value = eval(valueToCalculate);
+      setValueToCalculate(value);
+    }else if(name === 'clear'){
+      setValueToCalculate("");
+    }else{
+      setValueToCalculate((prev)=>{return prev+e.target.value})
+    }
+  }
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>CALCULATOR APP</h1>
+     <div className='container'>
+        <div className='calculatorscreen'>
+          <input value={valueToCalculate} className='screen'/>
+        </div>
+        <div className='allButton'>
+          {functionButton.map((fB)=>{
+            return(
+              <button className='fb' name='function' value={fB} onClick={(e)=>handleClick(e)}>{fB}</button>
+            )
+          })}
+
+          {button.map((btnvalue,index)=>{
+              
+              return(
+                <button className='btn' name='number' value={btnvalue} onClick={(e)=>handleClick(e)}>{btnvalue}</button>
+              )
+              
+          })}
+          <button name='equal' className='equalButton' onClick={(e)=>handleClick(e)}>=</button>
+          <button name='clear' className='clearButton' onClick={(e)=>handleClick(e)} >c</button>
+        </div>
+     </div>
     </div>
   );
 }
